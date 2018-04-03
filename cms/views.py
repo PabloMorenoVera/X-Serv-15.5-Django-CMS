9c1c284fca5d8c4108ca1c1195f2446d4a1a37ed
Pablo Moreno Vera <pablomvera14@gmail.com>
2018-04-03 11:49:32 +0200

    Add files via upload

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pages
+import urllib.request
 
 # Create your views here.
+def lista(request):
+    listado = Pages.objects.all()
+    response = "Contenido del diccionario:\n<ul>"
+    for recurso in listado:
+        response += "<li>" + recurso.name + "</a>"
+    return HttpResponse(response)
